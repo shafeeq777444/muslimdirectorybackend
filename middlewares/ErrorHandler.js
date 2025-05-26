cd0e8dd8ea0ccd578962a8bc7e87104b545511f7
shafeeq777444 <kp.shafeeqmohamed@gmail.com>
2025-05-27 01:16:48 +0530

    commented errorhandlers

--- middlewares/ErrorHandler.js
+++ middlewares/ErrorHandler.js
@@ -1,7 +1,7 @@
-const ErrorHandler = (err,req, res,  next) => {
-    const status = err.status || 500;
-    const message = err.message || "internal server error";
+// const ErrorHandler = (err,req, res,  next) => {
+//     const status = err.status || 500;
+//     const message = err.message || "internal server error";
 
-    res.status(status).json({ message });
-};
-export default ErrorHandler
+//     res.status(status).json({ message });
+// };
+// export default ErrorHandler
--- server.mjs
+++ server.mjs
@@ -37,7 +37,7 @@ app.use("/api", allRoutes);
 // static data
 app.use('/uploads',express.static(path.join(currentAppPath,'uploads')))
 
-app.use(ErrorHandler);
+// app.use(ErrorHandler);
 const port = process.env.PORT;
 app.listen(port, () => {
     console.log(`working on port ${port}`);
